953f1e2d2d3628d9f8b2a0b5aaad08eb4e866c31
mahesh9287 <118291978+mahesh9287@users.noreply.github.com>
2022-12-24 10:55:37 +0530

    Add files via upload

---
+++ Mani.java
@@ -0,0 +1,14 @@
+class Mani{
+public static void main(String argu[]){
+int a=10,b=20,c=0,d;
+d=a+b;
+System.out.println(d);
+while(c<=b)
+{
+System.out.println(c);
+c++;
+}
+System.out.println("to print the numbers");
+}
+}
+
